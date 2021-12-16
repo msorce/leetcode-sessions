780d92db56e5056ef704961aaeac20fd26695782
msorce <marksorce@gmail.com>
2021-12-15 21:17:56 -0800

    find all numbers disspeared in array

--- 12-10-22/easy/findAllNumbersDisappearedInAnArray.js
+++ 12-10-22/easy/findAllNumbersDisappearedInAnArray.js
@@ -1,26 +1,17 @@
 /**
  Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
  */
-const findDisappearedNumbers = function (nums) {
-    // USE NEGATIVE MARKING
-
-    // loop through elements
-
-    // mark that you have been to an index
-    // take the element and subtract 1 to get index
-    // mark element at the corrisponding index as negative
-    // increment to next element => mark element at that element -1 index
-    // etc...
-    // for (num of nums) {
-    //     let idx = num - 1;
-    //     if (nums[idx] > 0) {
-    //         nums[idx] = -nums[idx];
-    //     }
-    // }
-    // console.log(nums)
+const findDisappearedNumbers = nums => {
+    let ref;
     for (let i = 0; i < nums.length; i++) {
-
+        ref = Math.abs(nums[i]) - 1; // why do we need to use absolute value here? so that we always find the proper index in bounds
+        nums[ref] = -Math.abs(nums[ref]); // mark by turning negative, even if negative
     }
-};
 
-console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])) // [5, 6]
+    return nums.reduce((acc, curr, index, array) => {
+        if (curr > 0) {
+            acc.push(index+1);
+        }
+        return acc;
+    }, [])
+};
